0df70db74d4514c0ce7b64f94bdecb5cf1c4dacc
Dylan Cruse <dylancruse@live.com>
2018-09-02 03:48:13 +0000

    Created initial Post and Comment models and added migration files

--- app/redditclone/models.py
+++ app/redditclone/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    body = models.CharField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+
