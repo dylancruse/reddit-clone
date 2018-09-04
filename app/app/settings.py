073a4a40e0229e47cbdf809b5a7fbe6515bfb2db
Dylan Cruse <dylancruse@live.com>
2018-09-04 05:40:38 +0000

    Add skeleton index template and render to index view

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'redditclone.apps.RedditcloneConfig'
+    'redditclone',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/redditclone/views.py
+++ app/redditclone/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Under Construction")
+    # return HttpResponse('Under Construction')
+    return render(request, 'index.html')
