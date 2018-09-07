749d4675d95bcd668a971cd977b18ab4cd1c35eb
Dylan Cruse <dylancruse@live.com>
2018-09-07 00:51:51 +0000

    Added skeleton templates/urls/views for Create and Detail, update Index to get and pass Post objects

--- app/redditclone/urls.py
+++ app/redditclone/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from redditclone import views
 
 urlpatterns = [
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        path('detail', views.detail, name='detail'),
+        path('create', views.create, name='create')
     ]
--- app/redditclone/views.py
+++ app/redditclone/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 def index(request):
-    # return HttpResponse('Under Construction')
-    return render(request, 'index.html')
+    posts = Post.objects.all().order_by('title')
+    return render(request, 'index.html', {
+        'posts': posts
+    })
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def create(request):
+    return render(request, 'create.html')
