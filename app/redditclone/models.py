8e9b70bd10f86ba23aa6d8429b257fb90b8c7ba8
Dylan Cruse <dylancruse@live.com>
2018-09-18 23:16:07 +0000

    Added User model, 'accounts/' auth urls, skeleton login template view, updated Post and Comment models and added migrations

--- app/redditclone/models.py
+++ app/redditclone/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=140)
     body = models.CharField(max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         _title = self.title
@@ -13,7 +15,8 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     body = models.CharField(max_length=500)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         _body = self.body
